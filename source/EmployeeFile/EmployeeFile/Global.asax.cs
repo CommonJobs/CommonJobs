eb3cdca40d939fb7bcdb2f864ed275e94f957aae
Andres Moschini <andresmoschini@gmail.com>
2012-01-28 13:35:09 -0300

    Added Employee entity and sample controller an views

--- source/EmployeeFile/EmployeeFile/Global.asax.cs
+++ source/EmployeeFile/EmployeeFile/Global.asax.cs
@@ -25,7 +25,7 @@ namespace EmployeeFile
 
             routes.MapRoute(
                 "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
+                "{controller}/{action}/{*id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
