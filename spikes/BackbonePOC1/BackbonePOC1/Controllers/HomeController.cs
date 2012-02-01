2ea4c50978a36113b5003d367911b1275af4befb
Andres Moschini <andresmoschini@gmail.com>
2012-02-01 08:58:31 -0300

    Adding JD patch

--- spikes/BackbonePOC1/BackbonePOC1/Controllers/HomeController.cs
+++ spikes/BackbonePOC1/BackbonePOC1/Controllers/HomeController.cs
@@ -16,5 +16,21 @@ namespace BackbonePOC1.Controllers
             return View();
         }
 
+        public JsonResult GetCategories()
+        {
+            return Json(
+                new object[]
+                {
+                    new { code = "All", description = "Mixed" },
+                    new { code = "Processor", description = "Processor" },
+                    new { code = "GraphicCard", description = "Graphic Card" },
+                    new { code = "Motherboard", description = "Motherboard" },
+                    new { code = "Desktop", description = "Desktop" },
+                    new { code = "Notebook", description = "Notebook" },
+                    new { code = "Server", description = "Server" },
+                    new { code = "Workstation", description = "Workstation" },
+                }
+                , JsonRequestBehavior.AllowGet);
+        }
     }
 }
