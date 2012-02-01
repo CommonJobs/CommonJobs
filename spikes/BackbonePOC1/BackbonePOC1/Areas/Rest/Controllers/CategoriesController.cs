8b1f21420af16e0d34e5bea5186cf8b71fb27971
Andres Moschini <andresmoschini@gmail.com>
2012-02-01 11:11:33 -0300

    Adding an area to represent rest services

---
+++ spikes/BackbonePOC1/BackbonePOC1/Areas/Rest/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackbonePOC1.Areas.Rest.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        [AcceptVerbs(HttpVerbs.Get)]
+        public JsonResult Get()
+        {
+            return Json(
+                new object[]
+                            {
+                                new { code = "All", description = "Mixed", searchResultsView = "List" },
+                                new { code = "Processor", description = "Processor", searchResultsView = "Grid" },
+                                new { code = "GraphicCard", description = "Graphic Card", searchResultsView = "Grid" },
+                                new { code = "Motherboard", description = "Motherboard" },
+                                new { code = "Desktop", description = "Desktop" },
+                                new { code = "Notebook", description = "Notebook" },
+                                new { code = "Server", description = "Server" },
+                                new { code = "Workstation", description = "Workstation" },
+                            },
+                JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ spikes/BackbonePOC1/BackbonePOC1/Areas/Rest/RestAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace BackbonePOC1.Areas.Rest
+{
+    public class RestAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Rest";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Rest_Get",
+                "rest/categories",
+                new { controller = "Categories", action = "Get" }
+            );
+        }
+    }
+}
