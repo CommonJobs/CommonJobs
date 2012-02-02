720587e56ec510a99f69952cb3f383846bfd3d39
Andres Moschini <andresmoschini@gmail.com>
2012-02-02 10:46:31 -0300

    Backbone POC: working on detail category view and selective category list item renderization

--- spikes/BackbonePOC1/BackbonePOC1/site/categories.js
+++ spikes/BackbonePOC1/BackbonePOC1/site/categories.js
@@ -32,35 +32,54 @@ $(function () {
         tagName: "li",
         template: _.template($('#item-template').html()),
         events: {
-            "click .categoryitem": "showDetails"
+            "click .categoryitem": "edit"
         },
         initialize: function () {
-            this.model.bind('change', this.render, this);
+            this.model.bind('change', this.onChange, this);
+        },
+        onChange: function (e) {
+            var changed = e.changedAttributes();
+            //it could be useful because I do not want to renderize in all cases
+            if (changed.hasOwnProperty("description") || changed.hasOwnProperty("editing")) {
+                this.render();
+            }
         },
         render: function () {
             $(this.el).html(this.template(this.model.toJSON()));
             return this;
         },
-        showDetails: function () {
+        edit: function () {
             var model = this.model;
             Categories.each(function (cat) {
                 cat.set({ editing: cat == model });
             });
+            var view = new CategoryDetailView({ model: model });
+            $("#edit-category").empty().append(view.render().el);
         }
     });
+
+    window.CategoryDetailView = Backbone.View.extend({
+        template: _.template($('#edit-category-template').html()),
+        render: function () {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        }
+    })
+
 
     window.AppView = Backbone.View.extend({
         el: $("#categoriesConfigurationApp"),
         events: {
         },
         initialize: function () {
-            Categories.bind('reset', this.addAll, this);
+            Categories.bind('reset', this.resetItems, this);
             Categories.fetch();
         },
         render: function () {
             //console.debug("render all");
         },
-        addAll: function () {
+        resetItems: function () {
+            $("#category-list").empty();
             Categories.each(function (cat) {
                 var view = new CategoryItemView({ model: cat });
                 $("#category-list").append(view.render().el);
