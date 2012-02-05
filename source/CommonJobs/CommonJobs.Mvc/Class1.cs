12cda096a823bab13a1dce9ec3d74408a4391546
Andres Moschini <andresmoschini@gmail.com>
2012-02-05 17:19:59 -0300

    JsonDotNetModelBinder is ready, but JsonDotNetValueProviderFactory has a bug

--- source/CommonJobs/CommonJobs.Mvc/Class1.cs
+++ source/CommonJobs/CommonJobs.Mvc/Class1.cs
@@ -12,156 +12,60 @@ namespace System.Web.Mvc
 {
     public class JsonDotNetModelBinder : DefaultModelBinder
     {
-        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        //public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        //{
+            //if (bindingContext.ModelName == "InitialSalary" || bindingContext.ModelName == "Happenings[0].Salary" || bindingContext.ModelName == "Happenings[0].RealDate")
+            //{
+                //ConvertProviderResult(bindingContext.ModelState, bindingContext.ModelName, bindingContext.UnvalidatedValueProvider.GetValue(bindingContext.ModelName), bindingContext.ModelType);
+                //A first chance exception of type 'System.InvalidOperationException' occurred in System.Web.Mvc.dll
+                //base {System.SystemException}: {"The parameter conversion from type 'System.Int64' to type 'System.Decimal' failed because no type converter can convert between these types."}
+            //}
+        //    return base.BindModel(controllerContext, bindingContext);
+        //}
+
+        public bool PersonalizedBehaviorApplicable(ModelBindingContext bindingContext, out Type realType)
         {
             //TODO: remove restrictions ¿?
             if (bindingContext.ModelMetadata.IsComplexType && !bindingContext.ModelMetadata.ModelType.IsArray && !bindingContext.ModelMetadata.ModelType.IsGenericType)
             {
-                var realType = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".$type");
-                if (realType != null)
+                var realTypeValue = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".$type");
+                if (realTypeValue != null)
                 {
                     var modelType = bindingContext.ModelType;
-                    var type = Type.GetType(realType.RawValue.ToString());
-                    if (modelType != type && modelType.IsAssignableFrom(type))
-                    {
-                        //var myBindingContext = CreateComplexElementalModelBindingContext(controllerContext, bindingContext, null);
-                        /*
-                        BindAttribute bindAttr = (BindAttribute)GetTypeDescriptor(controllerContext, bindingContext).GetAttributes()[typeof(BindAttribute)];
-                        Predicate<string> newPropertyFilter = (bindAttr != null)
-                            ? propertyName => bindAttr.IsPropertyAllowed(propertyName) && bindingContext.PropertyFilter(propertyName)
-                            : bindingContext.PropertyFilter;
-                        */
-                        //var md = ModelMetadataProviders.Current.GetMetadataForType(
-                        //    () => collection, type);
-
-                        ModelBindingContext newBindingContext = new ModelBindingContext()
-                        {
-                            ModelMetadata = bindingContext.ModelMetadata,
-                            ModelName = bindingContext.ModelName,
-                            ModelState = bindingContext.ModelState,
-                            //PropertyFilter = newPropertyFilter,
-                            PropertyFilter = bindingContext.PropertyFilter,
-                            ValueProvider = bindingContext.ValueProvider
-                        };
-
-                        return base.BindModel(controllerContext, newBindingContext);
-                    }
+                    realType = Type.GetType(realTypeValue.RawValue.ToString());
+                    if (modelType != realType && modelType.IsAssignableFrom(realType))
+                        return true;
                 }
             }
-
-            return base.BindModel(controllerContext, bindingContext);
-        }
-
-        protected override void BindProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, ComponentModel.PropertyDescriptor propertyDescriptor)
-        {
-            base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+            realType = null;
+            return false;
         }
 
         protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
         {
-            //I am working here
-        //internal void BindComplexElementalModel(ControllerContext controllerContext, ModelBindingContext bindingContext, object model) {
-        //    // need to replace the property filter + model object and create an inner binding context
-        //    ModelBindingContext newBindingContext = CreateComplexElementalModelBindingContext(controllerContext, bindingContext, model);
+            Type realType;
+            if (PersonalizedBehaviorApplicable(bindingContext, out realType))
+                return base.CreateModel(controllerContext, bindingContext, realType);
 
-        //    // validation
-        //    if (OnModelUpdating(controllerContext, newBindingContext)) {
-        //        BindProperties(controllerContext, newBindingContext);
-        //        OnModelUpdated(controllerContext, newBindingContext);
-        //    }
-        //}
-            //TODO: remove restrictions ¿?
-            if (bindingContext.ModelMetadata.IsComplexType && !bindingContext.ModelMetadata.ModelType.IsArray && !bindingContext.ModelMetadata.ModelType.IsGenericType)
-            {
-                var realType = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".$type");
-                if (realType != null)
-                {
-                    var type = Type.GetType(realType.RawValue.ToString());
-                    if (modelType != type && modelType.IsAssignableFrom(type))
-                    {
-                        return base.CreateModel(controllerContext, bindingContext, type);
-                    }
-                }
-            }
             return base.CreateModel(controllerContext, bindingContext, modelType);
         }
 
         protected override ComponentModel.PropertyDescriptorCollection GetModelProperties(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
-            if (bindingContext.ModelMetadata.IsComplexType && !bindingContext.ModelMetadata.ModelType.IsArray && !bindingContext.ModelMetadata.ModelType.IsGenericType)
+            Type realType;
+            if (PersonalizedBehaviorApplicable(bindingContext, out realType))
             {
-                var realType = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".$type");
-                if (realType != null)
+                ModelBindingContext newBindingContext = new ModelBindingContext()
                 {
-                    var modelType = bindingContext.ModelType;
-                    var type = Type.GetType(realType.RawValue.ToString());
-                    if (modelType != type && modelType.IsAssignableFrom(type))
-                    {
-                        var md = ModelMetadataProviders.Current.GetMetadataForType(
-                            () => bindingContext.Model, type);
-
-                        ModelBindingContext newBindingContext = new ModelBindingContext()
-                        {
-                            ModelMetadata = md,
-                            ModelName = bindingContext.ModelName,
-                            ModelState = bindingContext.ModelState,
-                            //PropertyFilter = newPropertyFilter,
-                            PropertyFilter = bindingContext.PropertyFilter,
-                            ValueProvider = bindingContext.ValueProvider
-                        };
-                        return base.GetModelProperties(controllerContext, newBindingContext);
-                    }
-                }
+                    ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(() => bindingContext.Model, realType),
+                    ModelName = bindingContext.ModelName,
+                    ModelState = bindingContext.ModelState,
+                    PropertyFilter = bindingContext.PropertyFilter,
+                    ValueProvider = bindingContext.ValueProvider
+                };
+                return base.GetModelProperties(controllerContext, newBindingContext);
             }
             return base.GetModelProperties(controllerContext, bindingContext);
-        }
-
-        protected override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, ComponentModel.PropertyDescriptor propertyDescriptor, IModelBinder propertyBinder)
-        {
-            var value = base.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder);
-            return value;
-        }
-
-        protected override ComponentModel.ICustomTypeDescriptor GetTypeDescriptor(ControllerContext controllerContext, ModelBindingContext bindingContext)
-        {
-            //it does not work
-            //if (bindingContext.ModelMetadata.IsComplexType && !bindingContext.ModelMetadata.ModelType.IsArray)
-            //{
-            //    var realType = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + ".$type");
-            //    if (realType != null)
-            //    {
-            //        var type = Type.GetType(realType.RawValue.ToString());
-            //        if (bindingContext.ModelType.IsAssignableFrom(type))
-            //            return new AssociatedMetadataTypeTypeDescriptionProvider(type).GetTypeDescriptor(type);
-            //    }
-            //}
-
-            return base.GetTypeDescriptor(controllerContext, bindingContext);
-        }
-
-        protected override void OnModelUpdated(ControllerContext controllerContext, ModelBindingContext bindingContext)
-        {
-            base.OnModelUpdated(controllerContext, bindingContext);
-        }
-
-        protected override bool OnModelUpdating(ControllerContext controllerContext, ModelBindingContext bindingContext)
-        {
-            return base.OnModelUpdating(controllerContext, bindingContext);
-        }
-
-        protected override void OnPropertyValidated(ControllerContext controllerContext, ModelBindingContext bindingContext, ComponentModel.PropertyDescriptor propertyDescriptor, object value)
-        {
-            base.OnPropertyValidated(controllerContext, bindingContext, propertyDescriptor, value);
-        }
-
-        protected override bool OnPropertyValidating(ControllerContext controllerContext, ModelBindingContext bindingContext, ComponentModel.PropertyDescriptor propertyDescriptor, object value)
-        {
-            return base.OnPropertyValidating(controllerContext, bindingContext, propertyDescriptor, value);
-        }
-
-        protected override void SetProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, ComponentModel.PropertyDescriptor propertyDescriptor, object value)
-        {
-            base.SetProperty(controllerContext, bindingContext, propertyDescriptor, value);
         }
     }
 
