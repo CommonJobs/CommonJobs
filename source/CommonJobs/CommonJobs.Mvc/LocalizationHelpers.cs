ce1e3bf915aaecf0fa5cba33b36dd7cbeeeb7aeb
Andres Moschini <andresmoschini@gmail.com>
2012-02-18 10:20:03 -0300

    Adding localization helper and jQuery UI i18n. Auto culture in web.config

---
+++ source/CommonJobs/CommonJobs.Mvc/LocalizationHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommonJobs.Mvc
+{
+    public static class LocalizationHelpers
+    {
+        public static IHtmlString MetaAcceptLanguage<T>(this HtmlHelper<T> html)
+        {
+            var acceptLanguage = HttpUtility.HtmlAttributeEncode(System.Threading.Thread.CurrentThread.CurrentUICulture.ToString());
+            return new HtmlString(String.Format("<meta name=\"accept-language\" content=\"{0}\">", acceptLanguage));
+        }
+    }
+}
