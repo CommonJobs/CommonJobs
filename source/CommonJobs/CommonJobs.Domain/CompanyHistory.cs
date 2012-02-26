aa842bb3541175df8fdbf9da0c568edcbdabb223
Alpha <alphagma@gmail.com>
2012-02-26 13:24:18 -0600

    Added missing files for fixing project build.

---
+++ source/CommonJobs/CommonJobs.Domain/CompanyHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonJobs.Domain
+{
+    public class CompanyHistory
+    {
+        public string CompanyName { get; set; }
+        public bool IsCurrent { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
