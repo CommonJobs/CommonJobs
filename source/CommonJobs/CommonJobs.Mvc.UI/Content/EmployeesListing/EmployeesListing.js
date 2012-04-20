7ed0b4f229f06aa069c3c0dd5b3351efdb81f940
Andres Moschini <andresmoschini@gmail.com>
2012-04-20 08:05:16 -0300

    Move Applicants and Employees list javascript to independent files

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/EmployeesListing/EmployeesListing.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+
+    $(".results").on("click", ".card", function (e) {
+        e.preventDefault();
+        window.location = $(this).find(".card-link").attr("href");
+    });
+
+    function setResultCount() {
+        $("#resultCount").html($(".card").length - 1);
+    }
+
+    var loadResultsXHR = null;
+    $(".results").ajaxSend(function (event, jqXHR, ajaxOptions) {
+        if (loadResultsXHR)
+            loadResultsXHR.abort();
+        loadResultsXHR = jqXHR;
+    });
+
+    function search(ajax) {
+        //TODO Fix hardcoded URLs
+
+        var query = "?term=" + encodeURI($("#quickSearch").val());
+        if ($("#SearchInAttachmentsCheck").prop("checked"))
+            query += "&SearchInAttachments=true";
+        if ($("#SearchInNotesCheck").prop("checked"))
+            query += "&SearchInNotes=true";
+
+        if (ajax) {
+            $(".results").load("/Employees/List" + query, null, setResultCount);
+        } else {
+            location.href = "/Employees" + query;
+        }
+    }
+
+    $("#quickSearch").keyup(function (e) {
+        search(e.keyCode != 13);
+    });
+
+    $("#SearchInAttachmentsCheck, #SearchInNotesCheck").change(function () {
+        search(true);
+    });
+
+    $("#quickSearchSubmit").click(function () {
+        search(false);
+    });
+
+    setResultCount();
+});
