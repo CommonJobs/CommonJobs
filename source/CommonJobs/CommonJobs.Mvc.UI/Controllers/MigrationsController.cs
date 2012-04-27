e9a03ed2a9a17b082408e94946da9078de0e9361
Andres Moschini <andresmoschini@gmail.com>
2012-04-27 13:30:28 -0300

    Add empty migrator UI to CommonJobs.Mvc.UI

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/MigrationsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommonJobs.Raven.Migrations;
+using CommonJobs.Raven.Mvc;
+using CommonJobs.Migrations;
+using Miscellaneous.Attributes.Controller;
+
+namespace CommonJobs.Mvc.UI.Controllers
+{
+    public class MigrationsController : CommonJobsController
+    {
+        //
+        // GET: /Migrations
+        [FilterIP(
+            ConfigurationKeyAllowedMaskedIPs="AllowedMigrationMaskedIPs", 
+            ConfigurationKeyAllowedSingleIPs="AllowedMigrationSingleIPs",
+            ConfigurationKeyDeniedMaskedIPs="DeniedMigrationMaskedIPs",
+            ConfigurationKeyDeniedSingleIPs="DeniedMigrationSingleIPs")]
+        public ActionResult Index(string id)
+        {
+            var migrator = new Migrator(RavenSessionManager.DocumentStore, typeof(MigrationClass).Assembly);
+            var migrations = migrator.GetMigrationStatus();
+
+            return View();
+        }
+
+    }
+}
