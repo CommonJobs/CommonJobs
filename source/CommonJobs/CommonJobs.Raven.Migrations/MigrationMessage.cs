7f0c2007d972451c49f962b40dbb78f1047b7d3b
Andres Moschini <andresmoschini@gmail.com>
2012-05-01 10:00:45 -0300

    Log migration errors inside MigrationDescriptors

---
+++ source/CommonJobs/CommonJobs.Raven.Migrations/MigrationMessage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonJobs.Raven.Migrations
+{
+    public class MigrationMessage
+    {
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+        public MigrationActionType Action { get; set; }
+        public Exception Exception { get; set; }
+
+        public MigrationMessage()
+        {
+            Date = DateTime.Now;
+        }
+
+        public MigrationMessage(MigrationActionType action, string message)
+            : this()
+        {
+            Action = action;
+            Message = message;
+        }
+
+        public MigrationMessage(MigrationActionType action, Exception exception)
+            : this(action, CreateMessage(exception))
+        {
+            Exception = exception;
+        }
+
+        private static string CreateMessage(Exception exception)
+        {
+            return string.Format("Exception {0}\nType: {1}\nStackTrace: {2}", exception.GetType(), exception.Message, exception.StackTrace);
+        }
+    }
+}
