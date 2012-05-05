1c78b40934eadcfec857a12000134ce791bc590f
Alpha <alphagma@gmail.com>
2012-05-05 15:26:36 -0500

    Fixed some merge stuff.
    - Fixed unit test not working because of updated Newtonsoft JSON package update
    and not updating the reference. Using NuGet associations to make that happen,
    associating the MVC.Test project.
    
    - Fixed RavenDB startup scripts not updated when libraries changed.

--- source/CommonJobs/tools/Delete Raven Server Data.cmd
+++ source/CommonJobs/tools/Delete Raven Server Data.cmd
@@ -1,3 +1,3 @@
-rd /S /Q ..\packages\RavenDB.1.0.638-Unstable\server\Data
-rd /S /Q ..\packages\RavenDB.1.0.638-Unstable\server\Logs
-rd /S /Q ..\packages\RavenDB.1.0.638-Unstable\server\Tenants
+rd /S /Q ..\Libs\RavenDB\Server\Data
+rd /S /Q ..\Libs\RavenDB\Server\Logs
+rd /S /Q ..\Libs\RavenDB\Server\Tenants
