ad0f579232448f8e12be0f3122b2c4ed482bfdf6
Andres Moschini <andresmoschini@gmail.com>
2012-05-14 10:41:21 -0300

    Remove use of Raven/DocumentsByEntityName in migrations and add a helper method to simplify migrations writing

--- source/CommonJobs/CommonJobs.Raven.Migrations/Migration.cs
+++ source/CommonJobs/CommonJobs.Raven.Migrations/Migration.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Raven.Client;
+using Raven.Abstractions.Data;
+using Raven.Json.Linq;
 
 namespace CommonJobs.Raven.Migrations
 {
@@ -12,5 +14,48 @@ namespace CommonJobs.Raven.Migrations
 
         public abstract void Up();
         public abstract void Down();
+
+        protected bool IndexExists(string index, int pageSize = 64)
+        {
+            int start = 0;
+            while (true)
+            {
+                var names = DocumentStore.DatabaseCommands.GetIndexNames(start, pageSize);
+                if (names.Length == 0)
+                    return false;
+                else if (names.Contains(index))
+                    return true;
+                start += pageSize;
+            }
+        }
+
+        protected void DoInResults(
+            Action<RavenJObject> action, 
+            string query = "*:*", 
+            int pageSize = 64, 
+            string index = "Dynamic", 
+            string[] includes = null)
+        {
+            int start = 0;
+            while (true)
+            {
+                var qry = new IndexQuery() 
+                { 
+                    Query = query, 
+                    PageSize = pageSize,
+                    Start = start //TODO: consider to use another thing in place of start
+                };
+
+                var results = DocumentStore.DatabaseCommands.Query(index, qry, includes);
+
+                if (results.Results.Count == 0)
+                    break;
+
+                foreach (var result in results.Results)
+                    action(result);
+
+                start += pageSize;
+            }
+        }
     }
 }
