af7f4b40d9353fdb0003c0cd03ca8b27ea4259e7
Alpha <alphagma@gmail.com>
2012-06-03 09:19:04 -0500

    Fixed parameter name in help and added filemask option.
    Now allowing to limit the set of files that will be taken in consideration when
    deleting old files, so that different backups or different files can be in the
    same directory without being deleted.

--- source/CommonJobs/BackupTool/deleteOldBackup.cmd
+++ source/CommonJobs/BackupTool/deleteOldBackup.cmd
@@ -7,16 +7,18 @@ REM need backup folder
 REM optional: amount of backups to preserve
 SET backupFolder=%1
 SET keepRecentDays=%2
+SET fileMask=%3
 
 IF "%backupFolder%" EQU "" GOTO :ERROR
 IF "%keepRecentDays%" EQU "" SET /a keepRecentDays = 10
+IF "%fileMask%" EQU "" SET fileMask=*.*
 
 REM Iterar por los nombres de archivo de la carpeta destino
 REM Contar los primeros N y eliminar los posteriores uno a uno,
 REM ordenandolos por fecha de creación más reciente primero
 
 REM http://stackoverflow.com/q/51054/147507
-FORFILES -p "%backupFolder%" -m *.* -d %keepRecentDays% -c "cmd /c del @path"
+FORFILES -p "%backupFolder%" -m %fileMask% -d %keepRecentDays% -c "cmd /c del @path"
 
 ECHO Finalizado.
 GOTO END
@@ -37,15 +39,18 @@ ECHO CommonJobs, pero puede utilizarse para otros fines.
 ECHO CUIDADO: Esta herramienta eliminara archivos.
 ECHO.
 ECHO Sintaxis: 
-ECHO deleteOldBackup.cmd backupPath [backupsToKeep]
+ECHO deleteOldBackup.cmd backupPath [keepRecentDays]
 ECHO.
 ECHO     backupPath      Path de la carpeta en donde se almacenan los backups
 ECHO                     de CommonJobs.
 ECHO     keepRecentDays  (Opcional) Limite de antiguedad (en dias) para
 ECHO                     preservar backups. Default: 10.
+ECHO     fileMask        (Opcional) Mascara para identificar archivos que seran
+ECHO                     tenidos en cuenta como backup.
 ECHO.
 ECHO Ejemplos:
 ECHO     deleteOldBackup.cmd C:\CommonJobsDEV\Backups 5
+ECHO     deleteOldBackup.cmd C:\CommonJobsDEV\Backups 5 CommonJobsDEV*.*
 ECHO     deleteOldBackup.cmd C:\CommonJobs\Backups
 GOTO END
 
