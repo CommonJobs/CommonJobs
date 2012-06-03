988bd1b8aa2407e57e7accb71e66d78df9d0ae5b
Alpha <alphagma@gmail.com>
2012-06-02 19:21:34 -0500

    Changed implementation of deletion script.
    Now using forfiles command to iterate over files, which allows to run a command
    for each of these. This command already allows to filter files to beusedm with
    modified date filter.

--- source/CommonJobs/BackupTool/deleteOldBackup.cmd
+++ source/CommonJobs/BackupTool/deleteOldBackup.cmd
@@ -6,25 +6,17 @@ setlocal
 REM need backup folder
 REM optional: amount of backups to preserve
 SET backupFolder=%1
-SET backupsToKeep=%2
+SET keepRecentDays=%2
 
-IF [%backupFolder%] EQU [] GOTO :ERROR
-IF [%backupsToKeep%] EQU [] SET backupsToKeep = 10
+IF "%backupFolder%" EQU "" GOTO :ERROR
+IF "%keepRecentDays%" EQU "" SET /a keepRecentDays = 10
 
 REM Iterar por los nombres de archivo de la carpeta destino
 REM Contar los primeros N y eliminar los posteriores uno a uno,
 REM ordenandolos por fecha de creación más reciente primero
 
-@ECHO ON
-SET /a counter=1
-FOR /F %%F IN ('DIR "%backupFolder%" /B /A-D /O-D /TC') DO (
-	REM this does not work, the value of the variables are freezed in a for loop
-	ECHO %%F
-	ECHO %counter%
-	REM IF %counter% GTR %backupsToKeep% (DEL %backupFolder%\%%F)
-	SET /a counter="%counter%+1"
-	ECHO %counter%
-)
+REM http://stackoverflow.com/q/51054/147507
+FORFILES -p "%backupFolder%" -m *.* -d %keepRecentDays% -c "cmd /c del @path"
 
 ECHO Finalizado.
 GOTO END
@@ -49,8 +41,8 @@ ECHO deleteOldBackup.cmd backupPath [backupsToKeep]
 ECHO.
 ECHO     backupPath      Path de la carpeta en donde se almacenan los backups
 ECHO                     de CommonJobs.
-ECHO     backupsToKeep   (Opcional) Cantidad de backups a preservar.
-ECHO                     Default: 10.
+ECHO     keepRecentDays  (Opcional) Limite de antiguedad (en dias) para
+ECHO                     preservar backups. Default: 10.
 ECHO.
 ECHO Ejemplos:
 ECHO     deleteOldBackup.cmd C:\CommonJobsDEV\Backups 5
