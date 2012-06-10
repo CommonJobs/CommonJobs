90203c01ac7d98fa286f94c14cc11126e2566630
Alpha <alphagma@gmail.com>
2012-06-10 17:41:11 -0500

    Added controller and main view for file search.
    The .less files had to be refactored so that the common parts of a search are
    now avaialble through a ListingBase.less file.

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/FilesListing/FilesListing.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    var qs = new QuickSearchPage({
+        //pageSize: 3,
+        generateRedirectUrl: function (searchParameters) {
+            return urlGenerator.action("Index", "Files", searchParameters);
+        },
+        generateSearchUrl: function (searchParameters) {
+            return urlGenerator.action("List", "Files", searchParameters);
+        },
+        fillOtherSearchParameters: function (searchParameters) {
+            if ($("#SearchInAttachmentsCheck").prop("checked"))
+                searchParameters.searchInAttachments = true;
+            if ($("#SearchInNotesCheck").prop("checked"))
+                searchParameters.searchInNotes = true;
+        }
+    });
+
+    $("#quickSearchSubmit").click(function () {
+        //It also catches "enter"s in form inputs
+        qs.redirect();
+    });
+
+    $(".results").on("click", ".clickable", function (e) {
+        e.preventDefault();
+        window.location = $(this).find(".clickable-link").attr("href");
+    });
+
+    qs.search();
+});
---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/FilesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommonJobs.Raven.Mvc;
+
+namespace CommonJobs.Mvc.UI.Controllers
+{
+    [Authorize]
+    public class FilesController : CommonJobsController
+    {
+        public ActionResult Index(string searchTerm)
+        {
+            //TODO do search
+            return View(searchTerm);
+        }
+    }
+}
