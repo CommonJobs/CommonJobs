afb6bef083c189160aee3e9b2873ae80aba9020e
Alpha <alphagma@gmail.com>
2012-06-16 19:40:41 -0500

    Now reusing attachments controller

--- source/CommonJobs/CommonJobs.Mvc.UI/Controllers/AttachmentsController.cs
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/AttachmentsController.cs
@@ -145,5 +145,32 @@ namespace CommonJobs.Mvc.UI.Controllers
                 TotalResults = query.Stats.TotalResults
             });
         }
+
+        [Authorize]
+        public ActionResult Index(string searchTerm)
+        {
+            return View(searchTerm);
+        }
+
+        public JsonResult List(string searchTerm)
+        {
+            //TODO: real search
+            var results = new List<Attachment>() {
+                new Attachment("employees/1", "test1.pdf", "000001"),
+                new Attachment("employees/1", "test1.png", "000002"),
+                new Attachment("employees/1", "test1.gif", "000003"),
+                new Attachment("employees/2", "test1.docx", "000004"),
+                new Attachment("employees/2", "test1.xls", "000005"),
+                new Attachment("employees/2", "test1.txt", "000006"),
+                new Attachment("employees/2", "test1.jpg", "000007"),
+            };
+
+            return Json(new
+            {
+                Items = results,
+                //Skipped = ...
+                //TotalResults = ...
+            }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
