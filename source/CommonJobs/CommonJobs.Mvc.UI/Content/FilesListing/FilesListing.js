a149684f0870248d011df2f06ae0e58fec6bc17b
Alpha <alphagma@gmail.com>
2012-06-16 21:50:51 -0500

    Made template dynamic for file listing.

--- source/CommonJobs/CommonJobs.Mvc.UI/Content/FilesListing/FilesListing.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/FilesListing/FilesListing.js
@@ -1,13 +1,15 @@
-﻿//TODO uncomment
-/*
+﻿/// <reference path="../../Scripts/jquery-1.7.1-vsdoc.js" />
+/// <reference path="../../Scripts/underscore.js" />
+/// <reference path="../../Scripts/url-generator.js" />
+
 $(function () {
     var qs = new QuickSearchPage({
         //pageSize: 3,
         generateRedirectUrl: function (searchParameters) {
-            return urlGenerator.action("Index", "Files", searchParameters);
+            return urlGenerator.action("Index", "Attachments", searchParameters);
         },
         generateSearchUrl: function (searchParameters) {
-            return urlGenerator.action("List", "Files", searchParameters);
+            return urlGenerator.action("AttachmentsQuickSearch", "Attachments", searchParameters);
         },
         fillOtherSearchParameters: function (searchParameters) {
             //nothing to do here, the function is left for future addition of options
@@ -27,4 +29,32 @@ $(function () {
     qs.search();
 });
 
-*/
+var FileSearchUtilities = {
+    fileIconFromExtension: function(fileName) {
+        if (fileName == null) return urlGenerator.content("Images/filetypes/unknown.png");
+
+        var extensionSeparatorLocation = fileName.lastIndexOf('.');
+        var extension = fileName.substring(extensionSeparatorLocation + 1);
+        var allowedExtensions = ['avi', 'bmp', 'doc', 'docx', 'gif', 'jpeg', 'jpg', 'mov', 'mp3',
+            'mpeg', 'mpg', 'pdf', 'png', 'ppt', 'pptx', 'rar', 'txt', 'wmv', 'xls', 'xlsx', 'zip'];
+
+        var iconName = _.find(allowedExtensions, function (aExt) {
+            return aExt.toUpperCase() == extension.toUpperCase();
+        });
+
+        if (iconName == null) iconName = "unknown";
+
+        return urlGenerator.content("Images/filetypes/" + iconName + ".png");
+    },
+    urlToRelatedEntity: function (relatedEntityId, relatedEntityType) {
+        if (relatedEntityType.indexOf("Applicant") != -1) {
+            return urlGenerator.action("Edit", "Applicants", relatedEntityId);
+        }
+
+        if (relatedEntityType.indexOf("Employee") != -1) {
+            return urlGenerator.action("Edit", "Employees", relatedEntityId);
+        }
+
+        return null;
+    }
+};
--- source/CommonJobs/CommonJobs.Mvc.UI/Controllers/AttachmentsController.cs
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/AttachmentsController.cs
@@ -147,30 +147,9 @@ namespace CommonJobs.Mvc.UI.Controllers
         }
 
         [Authorize]
-        public ActionResult Index(string searchTerm)
+        public ActionResult Index(AttachmentSearchParameters searchParameters)
         {
-            return View(searchTerm);
-        }
-
-        public JsonResult List(string searchTerm)
-        {
-            //TODO: real search
-            var results = new List<Attachment>() {
-                new Attachment("employees/1", "test1.pdf", "000001"),
-                new Attachment("employees/1", "test1.png", "000002"),
-                new Attachment("employees/1", "test1.gif", "000003"),
-                new Attachment("employees/2", "test1.docx", "000004"),
-                new Attachment("employees/2", "test1.xls", "000005"),
-                new Attachment("employees/2", "test1.txt", "000006"),
-                new Attachment("employees/2", "test1.jpg", "000007"),
-            };
-
-            return Json(new
-            {
-                Items = results,
-                //Skipped = ...
-                //TotalResults = ...
-            }, JsonRequestBehavior.AllowGet);
+            return View(searchParameters ?? new AttachmentSearchParameters());
         }
     }
 }
