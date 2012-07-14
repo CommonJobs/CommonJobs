11373e5974b3d81118a17c28f5c5110e0c5983d0
Andres Moschini <andresmoschini@gmail.com>
2012-07-14 12:38:22 -0300

    Use CleanTextBuilder in IFilter extractor

--- source/CommonJobs/CommonJobs.ContentExtraction.IFilterExtraction/FilterContentExtractor.cs
+++ source/CommonJobs/CommonJobs.ContentExtraction.IFilterExtraction/FilterContentExtractor.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using CommonJobs.ContentExtraction.IFilterExtraction;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using CommonJobs.Utilities;
 
 namespace CommonJobs.ContentExtraction.Extractors
 {
@@ -11,17 +14,21 @@ namespace CommonJobs.ContentExtraction.Extractors
     {
         public bool TryExtract(string fullPath, Stream stream, string fileName, out ExtractionResult result)
         {
-            result = null;
+            var builder = new CleanTextBuilder();
             var reader = new FilterReader(fullPath, fileName);
             if (reader.Filtered)
             {
+                builder.Add(reader);
+
                 result = new ExtractionResult()
                 {
                     ContentType = null,
-                    PlainContent = reader.ReadToEnd()
+                    PlainContent = builder.ToString()
                 };
+
                 return true;
             }
+            result = null;
             return false;
         }
     }
