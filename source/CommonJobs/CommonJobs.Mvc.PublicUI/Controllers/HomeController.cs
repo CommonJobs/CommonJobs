869b2aca1bbc3f3ce5ebd64f0fd213fbee61c2f9
Andres Moschini <andresmoschini@gmail.com>
2012-07-21 12:20:06 -0300

    Create PublicUI project (files with my changes)

---
+++ source/CommonJobs/CommonJobs.Mvc.PublicUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommonJobs.Infrastructure.AttachmentStorage;
+using CommonJobs.Raven.Mvc;
+
+namespace CommonJobs.Mvc.PublicUI.Controllers
+{
+    public class HomeController : CommonJobsController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Attach(string id)
+        {
+            var entity = RavenSession.Load<object>(id);
+            if (entity == null)
+                return HttpNotFound("Specified entity does not exists");
+
+            var attachmentReader = new RequestAttachmentReader(Request);
+            var attachment = ExecuteCommand(new SaveAttachment(
+                entity,
+                attachmentReader.FileName,
+                attachmentReader.Stream));
+
+            return Json(new { success = true, attachment = attachment });
+        }
+    }
+}
---
+++ source/CommonJobs/CommonJobs.Mvc.PublicUI/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CommonJobs.ContentExtraction;
+using CommonJobs.ContentExtraction.Extractors;
+using CommonJobs.Infrastructure.Indexes;
+using CommonJobs.Raven.Mvc;
+
+namespace CommonJobs.Mvc.PublicUI
+{
+    public class MvcApplication : CommonJobsApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{*id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected override System.Reflection.Assembly[] GetIndexAssemblies()
+        {
+            return new[] { typeof(NullIndex).Assembly };
+        }
+
+        protected void Application_Start()
+        {
+            //TODO: move it to a common place in order to have the same configuration in CommonJobs.Mvc.UI and CommonJobs.Mvc.PublicUI
+            ContentExtractionConfiguration.Current.Clear();
+            ContentExtractionConfiguration.Current.AddRange(new IContentExtractor[] {
+                new PlainTextContentExtractor(), 
+                new FilterContentExtractor()
+            });
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
