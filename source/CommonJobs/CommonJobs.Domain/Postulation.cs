35f982d82bd108bb0bc9ff85ad6c4c579fd8c8a3
Andres Moschini <andresmoschini@gmail.com>
2012-07-23 09:20:16 -0300

    Working on postulation form

---
+++ source/CommonJobs/CommonJobs.Domain/Postulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace CommonJobs.Domain
+{
+    public class Postulation
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+
+        public TemporalFileReference Curriculum { get; set; }
+    }
+}
---
+++ source/CommonJobs/CommonJobs.Mvc.PublicUI/Controllers/PostulationsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommonJobs.Domain;
+using CommonJobs.Raven.Mvc;
+
+namespace CommonJobs.Mvc.PublicUI.Controllers
+{
+    public class PostulationsController : CommonJobsController
+    {
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        private TemporalFileReference GenerateFileReference(HttpPostedFileBase file)
+        {
+            var temporalFolderPath = System.Configuration.ConfigurationManager.AppSettings["CommonJobs/TemporalUploadsPath"];
+            var internalFileName = Guid.NewGuid().ToString();
+            var temporalFullName = Path.Combine(temporalFolderPath, internalFileName);
+            file.SaveAs(temporalFullName);
+            return new TemporalFileReference() 
+            {
+                OriginalFileName = file.FileName, //TODO: verify in different browsers if it is only the last part of the name
+                InternalFileName = internalFileName
+            };
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Postulation postulation, HttpPostedFileBase curriculumFile)
+        {
+            try
+            {
+                //Validate
+                postulation.Curriculum = GenerateFileReference(curriculumFile);
+                // TODO: Add insert logic here
+                //Convert to Applicant, asyncrlonusly?
+                return RedirectToAction("index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
