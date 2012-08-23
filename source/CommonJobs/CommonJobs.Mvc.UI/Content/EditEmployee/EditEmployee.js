ff4f2cd3b9498f9b3f03289c70e09b525ae119c8
Alpha <alphagma@gmail.com>
2012-08-22 20:27:32 -0500

    Showing modal screen for employees, using dropzone.

--- source/CommonJobs/CommonJobs.Mvc.UI/Content/EditEmployee/EditEmployee.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/EditEmployee/EditEmployee.js
@@ -4,6 +4,125 @@
 /// <reference path="../../Scripts/url-generator.js" />
 
 (function () {
+    var dragAndDrop = new DragAndDrop();
+
+    //Attachments utilities
+    var getEmployeeAttachmentsBySlot = function (employee) {
+        var filledById = {};
+        if (employee) {
+            _.chain(employee.AttachmentsBySlot)
+            .filter(function (filledItem) {
+                return !!filledItem.Attachment;
+            })
+            .each(function (filledItem) {
+                filledById[filledItem.SlotId] = filledItem;
+            });
+        }
+        return filledById;
+    };
+
+    //Extend UploadModal
+    var slotBtnTemplate = _.template($("#slot-button-template").text());
+    var slotsTemplate = _.template($("#available-slots-template").text());
+    var previousInit = UploadModal.prototype._init;
+    UploadModal.prototype._init = function ($modal) {
+        _.bind(previousInit, this)($modal);
+        this.$(".slots").empty();
+        this.closeButtonText("Cerrar");
+    };
+    UploadModal.prototype.drawSlots = function ($el, employee) {
+        var me = this;
+
+        var filledById = getEmployeeAttachmentsBySlot(employee);
+
+        var singleFile = this._files.length == 1;
+        var $slots = $(slotsTemplate({ model: { singleFile: singleFile } }));
+
+        if (singleFile) {
+            var btns = {};
+
+            _.each(ViewData.attachmentSlots, function (slot) {
+                var filled = filledById[slot.Id];
+                var $btn;
+                if (filled && filled.Attachment) {
+                    $btn = $(slotBtnTemplate({ model: { caption: slot.Name + ": " + filled.Attachment.FileName } }));
+                    $btn.prop("disabled", true);
+                } else {
+                    $btn = $(slotBtnTemplate({ model: { caption: slot.Name } }));
+                    $btn.on("click", function () {
+                        me.data.formData = { slot: slot.Id };
+                        me.data.submit();
+                    });
+                }
+
+                var key = ".slots-necessity-" + slot.Necessity;
+                if (!btns[key])
+                    btns[key] = [];
+                btns[key].push($btn);
+            });
+
+            for (var key in btns) {
+                var group = $slots.find(key);
+                group.show();
+                var title = group.find("h5");
+                var $btn;
+                while ($btn = btns[key].shift()) {
+                    title.after($btn);
+                }
+            }
+        }
+
+        $slots.find(".slot-general").on("click", function () {
+            me.data.submit();
+        });
+
+        this.$(".slots").html($slots);
+
+        return this;
+    };
+    UploadModal.prototype.closeButtonText = function (text) {
+        this.$(".close-button", function () {
+            this.text(text);
+        });
+        return this;
+    }
+
+    var prepareAttachmentZone = function (dropZone, model) {
+        dragAndDrop.prepareFileDropzone(dropZone, {
+            add: function (e, data, $el) {
+                if ($el.hasClass("files-data")) {
+                    new UploadModal($('#generic-modal'))
+                        .personDetail(model, $el)
+                        .title("Adjuntar Archivos")
+                        .files(data)
+                        .drawSlots($el, model)
+                        .closeButtonText("Cancelar")
+                        .modal();
+                }
+            },
+            done: function (e, data, $el) {
+                if (data.result.added) {
+                    if (model.AttachmentsBySlot == null)
+                        model.AttachmentsBySlot = [];
+                    model.AttachmentsBySlot.push(data.result.added);
+                }
+                new UploadModal($('#generic-modal'))
+                    .personDetail(model, $el)
+                    .title("Archivos subidos")
+                    .files(data)
+                    .modal();
+            },
+            fail: function (e, data, $el) {
+                new UploadModal($('#generic-modal'))
+                    .personDetail(model, $el)
+                    .title("Error subiendo archivos")
+                    .error()
+                    .files(data)
+                    .modal();
+            }
+        });
+    };
+
     var App = this.App = {};
 
     App.Note = Backbone.Model.extend({
@@ -344,6 +463,7 @@
         },
         initialize: function () {
             this.dataBinder = new App.EditEmployeeAppViewDataBinder({ el: this.el, model: this.model });
+            prepareAttachmentZone($(this.el).find(".files-data"), this.model.toJSON());
         },
         events: {
             "click .saveEmployee": "saveEmployee",
