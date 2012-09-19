7843b96984374a105af52f86257c28bcc2520042
Alpha <alphagma@gmail.com>
2012-09-18 21:18:24 -0500

    Added controller and view

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/EmployeeFilesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommonJobs.Mvc.UI.Controllers
+{
+    //NOTE: "Employee Files" as a name refers to the file that is the source of record for employee data
+    //It is NOT meant as an synonym to "attachment"
+    public class EmployeeFilesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
