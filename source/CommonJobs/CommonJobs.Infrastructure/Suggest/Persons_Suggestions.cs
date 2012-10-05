68c1e819cc29b35e1a48579a70c63860085b486c
Andres Moschini <andresmoschini@gmail.com>
2012-10-05 10:32:29 -0300

    Allow email and email domain suggestions

--- source/CommonJobs/CommonJobs.Infrastructure/Suggest/Persons_Suggestions.cs
+++ source/CommonJobs/CommonJobs.Infrastructure/Suggest/Persons_Suggestions.cs
@@ -13,6 +13,8 @@ namespace CommonJobs.Infrastructure.Persons
         public class Projection
         {
             public string College { get; set; }
+            public string Email { get; set; }
+            public string EmailDomain { get; set; }
             public string EntityType { get; set; }
             public string Id { get; set; }
         }
@@ -24,6 +26,19 @@ namespace CommonJobs.Infrastructure.Persons
                 select new
                 {
                     College = entity.College,
+                    Email = entity.Email,
+                    EmailDomain = entity.Email == null || !entity.Email.Contains("@") ? null : entity.Email.Split(new[] { '@' }, 2)[1],
+                    EntityType = "Employee",
+                    Id = entity.Id
+                });
+
+            AddMap<Employee>(employees =>
+                from entity in employees
+                select new
+                {
+                    College = entity.College,
+                    Email = entity.CorporativeEmail,
+                    EmailDomain = entity.CorporativeEmail == null || !entity.CorporativeEmail.Contains("@") ? null : entity.CorporativeEmail.Split(new[] { '@' }, 2)[1],
                     EntityType = "Employee",
                     Id = entity.Id
                 });
@@ -33,20 +48,26 @@ namespace CommonJobs.Infrastructure.Persons
                 select new
                 {
                     College = entity.College,
+                    Email = entity.Email,
+                    EmailDomain = entity.Email == null || !entity.Email.Contains("@") ? null : entity.Email.Split(new[] { '@' }, 2)[1],
                     EntityType = "Applicant",
                     Id = entity.Id
                 });
 
             Reduce = docs => from doc in docs
-                             group doc by doc.Id into g
+                             group doc by new { doc.Id, doc.Email } into g
                              select new
                              {
                                 College = g.Select(x => x.College).FirstOrDefault(),
+                                Email = g.Select(x => x.Email).FirstOrDefault(),
+                                EmailDomain = g.Select(x => x.EmailDomain).FirstOrDefault(),
                                 EntityType = g.Select(x => x.EntityType).FirstOrDefault(),
                                 Id = g.Select(x => x.Id).FirstOrDefault()
                              };
-            
+
             Index(x => x.College, FieldIndexing.Analyzed);
+            Index(x => x.Email, FieldIndexing.NotAnalyzed);
+            Index(x => x.EmailDomain, FieldIndexing.NotAnalyzed);
         }
     }
 }
--- source/CommonJobs/CommonJobs.Mvc.UI/Controllers/SuggestController.cs
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/SuggestController.cs
@@ -19,5 +19,27 @@ namespace CommonJobs.Mvc.UI.Controllers
             var results = Query(new GetSuggestions(x => x.College, term, maxSuggestions));
             return Json(new { suggestions = results });
         }
+
+        [CommonJobsAuthorize(Roles = "Users")]
+        public JsonNetResult Email(string term, int maxSuggestions = 10)
+        {
+            var results = Query(new GetSuggestions(x => x.Email, term, maxSuggestions));
+            return Json(new { suggestions = results });
+        }
+
+        public JsonNetResult EmailDomain(string term, int maxSuggestions = 10)
+        {
+            if (term == null || !term.Contains("@"))
+                return null; //TODO
+
+            var parts = term.Split(new[] { '@' }, 2);
+            var domainTerm = parts[1];
+            var prefix = parts[0];
+
+            var results = Query(new GetSuggestions(x => x.EmailDomain, domainTerm, maxSuggestions))
+                .Select(x => string.Format("{0}@{1}", prefix, x))
+                .ToArray();
+            return Json(new { suggestions = results });
+        }
     }
 }
