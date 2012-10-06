a231aab1c063d2b04cb956deb9c4a2f45b7e76ee
Andres Moschini <andresmoschini@gmail.com>
2012-10-06 09:42:39 -0300

    Add multiple suggestion to skills

--- source/CommonJobs/CommonJobs.Mvc.UI/Content/EditCommon/Suggest.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/EditCommon/Suggest.js
@@ -35,5 +35,41 @@
         });
     });
 
-    
+    function extractor(query) {
+        var result = /([^,]+)$/.exec(query);
+        if (result && result[1])
+            return result[1].trim();
+        return '';
+    }
+
+    $(".editable-field[data-cj-suggest-multiple]").each(function () {
+        var $el = $(this);
+        var key = $el.attr("data-cj-suggest-multiple");
+        var $input = $el.find(".editor-editable");
+        $input.typeahead({
+            source: function (query, process) {
+                var input = $input[0];
+                var term;
+                if (query.length == input.selectionStart && query.length == input.selectionEnd && (term = extractor(query))) {
+                    $.ajax({
+                        url: urlGenerator.action(key, "Suggest", { term: term }),
+                        success: function (data) {
+                            if (data && data.suggestions)
+                                process(data.suggestions);
+                        }
+                    });
+                }
+            }
+            , matcher: function () { return true; }
+            , updater: function (item) {
+                return this.$element.val().replace(/[^,\s]*$/, '') + item + ', ';
+            }
+            , highlighter: function (item) {
+                var query = extractor(this.query).replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
+                return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
+                    return '<strong>' + match + '</strong>'
+                })
+            }
+        });
+    });
 }
