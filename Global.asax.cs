aaa435777e5c8e736ce63bb4ffb11dfa1b4ecbb9
Andres Moschini <andresmoschini@gmail.com>
2012-10-11 14:12:01 -0300

    Add an html page and move mvc urls into mvc subfolder

--- Global.asax.cs
+++ Global.asax.cs
@@ -23,7 +23,7 @@ namespace CommonFood
 
             routes.MapRoute(
                 "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
+                "mvc/{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
