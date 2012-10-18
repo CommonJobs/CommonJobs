10aa081804fca5f8e87233ebc43d63f3ed6f9553
Andres Moschini <andresmoschini@gmail.com>
2012-10-18 08:40:13 -0300

    Add absence class to domain

---
+++ source/CommonJobs/CommonJobs.Domain/Absence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonJobs.Domain
+{
+    public class Absence : NoteWithAttachment
+    {
+        public string Reason { get; set; }
+        public DateTime To { get; set; }
+        public bool Partial { get; set; }
+        public bool HasCertificate { get; set; }
+    }
+}
