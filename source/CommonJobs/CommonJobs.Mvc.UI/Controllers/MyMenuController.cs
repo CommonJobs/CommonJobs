392446b87e0b86765f444548a5b9423dc21fa3a9
Andres Moschini <andresmoschini@gmail.com>
2012-11-17 09:31:49 -0300

    Add Controllers and move HTML to Views

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Controllers/MyMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommonJobs.Mvc.UI.Controllers
+{
+    public class MyMenuController : Controller
+    {
+        //El propio menu
+        public ActionResult Index()
+        {
+            return View("MyMenu");
+        }
+
+        //definición de los menues
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        //el menu de otro
+        public ActionResult Edit(string domainUser)
+        {
+            return View("MyMenu");
+        } 
+    }
+}
