7b84e32a8b92b9fa9622afbc7f7f730f2e10861e
Andres Moschini <andresmoschini@gmail.com>
2012-12-06 09:56:33 -0300

    SecurityTrimming Refactorization

---
+++ source/CommonJobs/CommonJobs.Infrastructure.Mvc/SecurityTrimming/SecurityTrimmingExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace CommonJobs.Infrastructure.Mvc.SecurityTrimming
+{
+    public static class SecurityTrimmingExtensions
+    {
+        public static SecurityTrimmingHelper GetSecurityTrimmingHelper(this HtmlHelper htmlHelper, string actionName, string controllerName = null)
+        {
+            return new SecurityTrimmingHelper(htmlHelper, actionName, controllerName);
+        }
+
+        public static SecurityTrimmingForm BeginSecurityTrimmingForm(this HtmlHelper htmlHelper, string actionName, string controllerName = null)
+        {
+            Func<MvcForm> lazyForm = () => htmlHelper.BeginForm(actionName, controllerName);
+            return new SecurityTrimmingForm(htmlHelper, lazyForm, actionName, controllerName);
+        }
+
+        public static HelperResult RenderIfHasPermission(this HtmlHelper htmlHelper, string actionName, Func<SecurityTrimmingHelper, HelperResult> template)
+        {
+            return RenderIfHasPermission(htmlHelper, actionName, null, template);
+        }
+
+        public static HelperResult RenderIfHasPermission(this HtmlHelper htmlHelper, string actionName, string controllerName, Func<SecurityTrimmingHelper, HelperResult> template)
+        {
+            using (var helper = new SecurityTrimmingHelper(htmlHelper, actionName, controllerName))
+            {
+                return helper.HasPermission ? template(helper) : new HelperResult(writer => { });
+            }
+        }
+    }
+}
