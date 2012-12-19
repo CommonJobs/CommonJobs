605bbcd6950517d39464f11b7471251f82fe7b7e
Andres Moschini <andresmoschini@gmail.com>
2012-12-19 17:12:40 -0300

    Show tooltips and popovers

--- source/CommonJobs/CommonJobs.Mvc.UI/Content/Absences/Absences.js
+++ source/CommonJobs/CommonJobs.Mvc.UI/Content/Absences/Absences.js
@@ -23,24 +23,27 @@ $(function () {
     var end = moment([year]).endOf("year").startOf('day').valueOf();
     
     while (current.valueOf() <= end) {
-        var weekday = current.day();
-        var index = "m" + current.month() + "d" + current.date();
-        var generateMData = function (index) {
-            return function (source, type, val) { return source.daysData[index] || null; };
-        };
-        columns.push({
-            bSortable: false,
-            sClass: "cell-day" + (weekday == 0 || weekday == 6 ? " weekend" : ""), 
-            mData: generateMData(index),
-            fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
-                //No puedo usar mRender porque me modifica el sData de fnCreatedCell
-                if (sData) {
-                    $(nTd)
-                        .addClass("absence " + sData.AbsenceType + " " + sData.ReasonSlug)
-                        .data("absenceData", sData);
-                }
-            }
-        });
+        //Necesito evaluar current ahora, no cuando se invocan las funciones
+        (function (current) {
+            var weekday = current.day();
+            columns.push({
+                bSortable: false,
+                sClass: "cell-day" + (weekday == 0 || weekday == 6 ? " weekend" : ""),
+                mData: function (source, type, val) {
+                    return source.daysData["m" + current.month() + "d" + current.date()] || null;
+                },
+                fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
+                    var $td = $(nTd);
+                    $td.data("current", current);
+                    //No puedo usar mRender porque me modifica el sData de fnCreatedCell
+                    if (sData) {
+                        $td
+                            .addClass("absence " + sData.AbsenceType + " " + sData.ReasonSlug)
+                            .data("absenceData", sData);
+                    }
+                },
+            });
+        })(current.clone());
         current.add('days', 1);
     }
 
@@ -73,8 +76,44 @@ $(function () {
         fnCreatedRow: function (nRow, aData, iDataIndex) {
             var $row = $(nRow);
             $row.data("absenceData", aData);
+            $row.find("td.cell-day:not(.absence)").tooltip({
+                html: true,
+                title: getTitle,
+                placement: "bottom"
+                //, delay: 500
+            });
+            $row.find("td.cell-day.absence").popover({
+                html: true,
+                title: getTitle,
+                placement: "bottom",
+                content: getContent
+            });
         }
     });
+
+    function getTitle() {
+        var $this = $(this);
+        var current = $this.data("current");
+        var oData = $this.parent("tr").data("absenceData");
+        return "" + oData.LastName + ", " + oData.FirstName + "<br />" + current.format("dddd D [de] MMMM YYYY");
+    }
+
+    function getContent() {
+        var $this = $(this);
+        var current = $this.data("current");
+        var sData = $this.data("absenceData");
+        var oData = $this.parent("tr").data("absenceData");
+        //TODO: complete it
+        return "<dl class='dl-horizontal'>" +
+            "<dt>" + "Razón:" + "</dt>" + "<dd>" + sData.Reason + "</dd>" +
+            "<dt>" + "Tipo:" + "</dt>" + "<dd>" + sData.AbsenceType + "</dd>" + //TODO: traducir
+            "<dt>" + "Desde:" + "</dt>" + "<dd>" + sData.RealDate + "</dd>" + //TODO: formatear y cambiar "Desde" por "Fecha"
+            "<dt>" + "Hasta:" + "</dt>" + "<dd>" + sData.To + "</dd>" + //TODO: formatear u ocultar o mostrar desde/hasta en una linea
+            "<dt>" + "Certificado:" + "</dt>" + "<dd>" + (sData.HasCertificate ? "Si" : "No") + "</dd>" + 
+            "<dt>" + "Adjunto:" + "</dt>" + "<dd>" + sData.Attachment + "</dd>" + //TODO: mostrar link? se me va el popover, como hago? con click?
+            "<dt>" + "Nota:" + "</dt>" + "<dd>" + sData.Note + "</dd>" + //TODO: markdown
+            "</dl>";
+    }
     
     var processor = new AjaxHelper.BunchProcessor(
         function (take, skip, callback) {
