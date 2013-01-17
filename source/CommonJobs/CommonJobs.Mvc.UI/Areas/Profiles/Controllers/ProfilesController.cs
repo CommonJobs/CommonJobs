b5cf4ddf3e5a8bdaaed75897691df5767131ca2e
Andres Moschini <andresmoschini@gmail.com>
2013-01-17 15:18:14 -0300

    Add "Profiles" area

---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Areas/Profiles/Controllers/ProfilesController.cs
@@ -0,0 +1,17 @@
+﻿using CommonJobs.Infrastructure.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommonJobs.Mvc.UI.Areas.Profiles
+{
+    public class ProfilesController : CommonJobsController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/CommonJobs/CommonJobs.Mvc.UI/Areas/Profiles/ProfilesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CommonJobs.Mvc.UI.Areas.Profiles
+{
+    public class ProfilesAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Profiles";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Profiles_default",
+                "Profiles/{controller}/{action}/{id}",
+                new { controller = "Profiles", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
