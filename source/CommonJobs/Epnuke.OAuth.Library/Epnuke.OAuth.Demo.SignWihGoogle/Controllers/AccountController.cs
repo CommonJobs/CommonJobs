9ff88c0ffef7f5cfab239b124449ac9f8e3b40dd
Andres Moschini <andresmoschini@gmail.com>
2013-02-01 09:40:58 -0300

    Add Google OAuth requests (it is not working)

--- source/CommonJobs/Epnuke.OAuth.Library/Epnuke.OAuth.Demo.SignWihGoogle/Controllers/AccountController.cs
+++ source/CommonJobs/Epnuke.OAuth.Library/Epnuke.OAuth.Demo.SignWihGoogle/Controllers/AccountController.cs
@@ -36,5 +36,48 @@ namespace Epnuke.OAuth.Demo.SignWihGoogle.Controllers
             ViewBag.MensajeError = "Credenciales inválidas";
             return View();
         }
+
+
+        public ActionResult LogOnGoogle()
+        {
+            // It is not working, I think that it could be usefull to check with https://github.com/niuchl/oauth-login-demo and https://developers.google.com/accounts/docs/OAuth2
+            // for example: https://github.com/niuchl/oauth-login-demo/blob/master/endpoints.py or https://github.com/niuchl/oauth-login-demo/blob/master/auth.py
+
+            var oauthSession = new OAuthClientSession(
+                ckey: "293136734985.apps.googleusercontent.com",
+                csecret: "replace", //TODO: reset on https://code.google.com/apis/console/b/1/?pli=1#project:293136734985:access
+                nonceGenerator: new NonceGenerator32Bytes() //TODO: Check if it is valid for google
+            ); 
+
+            // It is not working for many reasons:
+            // * The remote server returned an error: (411) Length Required.
+            // * The remote server returned an error: (400) Bad Request. (if I manually set ContentLenght to 0)
+            oauthSession.RequestTemporaryCredentials(
+                requestTokenUri: "https://accounts.google.com/o/oauth2/token", 
+                httpMethod: "POST", 
+                callback: "http://localhost:64984/Account/Callback" 
+            );
+
+            Session["oauth"] = oauthSession;
+            return Redirect(oauthSession.GetAuthorizationUri("https://accounts.google.com/o/oauth2/auth"));
+        }
+
+        public ActionResult Callback(string oauth_verifier)
+        {
+            var oauthSession = Session["oauth"] as OAuthClientSession;
+            oauthSession.Authorize(oauth_verifier, ConfigurationManager.AppSettings["access_token"]);
+
+            if (oauthSession.IsAuthorized)
+            {
+                var name = oauthSession.GetAdditionalData("screen_name");
+                FormsAuthentication.SetAuthCookie(name, false);
+                return RedirectToAction("Index", "Private");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
     }
 }
