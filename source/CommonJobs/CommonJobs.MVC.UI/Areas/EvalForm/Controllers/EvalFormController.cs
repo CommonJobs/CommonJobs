8be051bc74ee6e0b3596bd2eafc1c2273c25dec9
gkolocsar <gkolocsar@makingsense.com>
2015-06-09 11:50:20 -0300

    Basic BE structure

---
+++ source/CommonJobs/CommonJobs.MVC.UI/Areas/EvalForm/Controllers/EvalFormController.cs
@@ -0,0 +1,30 @@
+﻿using CommonJobs.Application.MyMenu;
+using CommonJobs.Domain;
+using CommonJobs.Domain.MyMenu;
+using CommonJobs.Infrastructure.Mvc;
+using CommonJobs.Mvc.UI.Infrastructure;
+using NLog;
+using Raven.Client.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommonJobs.Utilities;
+
+namespace CommonJobs.Mvc.UI.Areas.EvalForm
+{
+    [CommonJobsAuthorize]
+    //TODO: Add documentation
+    [Documentation("docs/manual-de-usuario/...")]
+    public class EvalFormController : CommonJobsController
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            //TODO: ...
+            return null;
+        }
+    }
+}
---
+++ source/CommonJobs/CommonJobs.MVC.UI/Areas/EvalForm/EvalFormAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CommonJobs.Mvc.UI.Areas.EvalForm
+{
+    public class EvalFormAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "EvalForm";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "EvalForm_default",
+                "EvalForm/{action}/{id}",
+                new { controller = "EvalForm", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
