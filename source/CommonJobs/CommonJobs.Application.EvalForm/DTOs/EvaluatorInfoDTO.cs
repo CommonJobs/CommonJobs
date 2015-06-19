34d49400f01a5a7cdcee566dce97ac5e7d4e728b
gkolocsar <gkolocsar@makingsense.com>
2015-06-19 16:27:48 -0300

    Adds Dtos and updates command for adding evaluatros.

---
+++ source/CommonJobs/CommonJobs.Application.EvalForm/DTOs/EvaluatorInfoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonJobs.Application.EvalForm.DTOs
+{
+    public class EvaluatorInfoDTO
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
