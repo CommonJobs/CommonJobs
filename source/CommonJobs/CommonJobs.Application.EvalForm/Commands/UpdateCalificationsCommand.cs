0d4470341604dfbeb0cf1aed4163b9f334506af6
gkolocsar <gkolocsar@makingsense.com>
2015-07-01 16:00:24 -0300

    *WIP*

---
+++ source/CommonJobs/CommonJobs.Application.EvalForm/Commands/UpdateCalificationsCommand.cs
@@ -0,0 +1,20 @@
+﻿using CommonJobs.Infrastructure.RavenDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonJobs.Application.EvalForm.Commands
+{
+    public class UpdateCalificationsCommand : Command
+    {
+        public UpdateCalificationsCommand()
+        {
+
+        }
+
+        public override void Execute()
+        {
+        }
+    }
+}
