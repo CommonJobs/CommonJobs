77a22d6bc08904088ccfd6706306cca6a5053d57
José <josegutierrezmdp@gmail.com>
2015-07-02 10:55:06 -0300

    Load items grid dynamically on Calification page

--- source/CommonJobs/CommonJobs.Application.EvalForm/DTOs/CalificationsDTO.cs
+++ source/CommonJobs/CommonJobs.Application.EvalForm/DTOs/CalificationsDTO.cs
@@ -8,16 +8,17 @@ namespace CommonJobs.Application.EvalForm.DTOs
 {
     public class CalificationsDTO
     {
-        public EmployeeEvaluationDTO Evaluation { get; set; }
+        public UserView View { get; set; }
+
+        public EmployeeEvaluation Evaluation { get; set; }
 
         public List<EvaluationCalification> Califications { get; set; }
+    }
 
-        public int UserView { get; set; }
-
-        public string StrengthsComment { get; set; }
-
-        public string ImproveComment { get; set; }
-
-        public string ActionPlanComment { get; set; }
+    public enum UserView
+    {
+        Auto,
+        Responsible,
+        Evaluation
     }
 }
