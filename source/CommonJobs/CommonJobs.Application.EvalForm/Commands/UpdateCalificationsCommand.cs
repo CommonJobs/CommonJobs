29858f2925e3e9c1b21d987041d868cd98f95802
gkolocsar <gkolocsar@makingsense.com>
2015-07-02 16:56:18 -0300

    Refactor and security. Now the user cannot forcibly save califications that aren't his/her responsibility.

--- source/CommonJobs/CommonJobs.Application.EvalForm/Commands/UpdateCalificationsCommand.cs
+++ source/CommonJobs/CommonJobs.Application.EvalForm/Commands/UpdateCalificationsCommand.cs
@@ -10,11 +10,17 @@ namespace CommonJobs.Application.EvalForm.Commands
 {
     public class UpdateCalificationsCommand : Command
     {
+        private const string COMPANY = "_company";
+
         private UpdateEvaluationDTO _updateEvaluation;
 
-        public UpdateCalificationsCommand(UpdateEvaluationDTO updateEvaluation)
+        private string _loggedUser;
+
+        public UpdateCalificationsCommand(UpdateEvaluationDTO updateEvaluation, string loggedUser)
         {
             _updateEvaluation = updateEvaluation;
+
+            _loggedUser = loggedUser;
         }
 
         public override void Execute()
@@ -38,58 +44,84 @@ namespace CommonJobs.Application.EvalForm.Commands
                     throw new ApplicationException(string.Format("Error: Calificación {0} inexistente para evaluación {1}", calification.CalificationId, _updateEvaluation.EvaluationId));
                 }
 
-                // Update the calification comments and values
-                storedCalification.Comments = calification.Comments;
-                storedCalification.Califications = calification.Items;
-
-                // Update the EvaluationCalification document in the collection (DB)
-                RavenSession.Store(storedCalification);
-
-                // If it's the responsible's calification and it's finished, then the company calification should be created
-                if (storedCalification.Owner == CalificationType.Responsible)
+                // Checks if the user trying to update the califications can actually do it
+                if (CanUpdate(_loggedUser, storedEvaluation, storedCalification))
                 {
-                    updateEvaluationProject = true;
+                    UpdateCalification(calification, storedCalification);
 
-                    if (storedCalification.Finished)
-                    {
-                        // Creates the company calification once the responsible one is finished. The evaluator still is the
-                        ExecuteCommand(new GenerateCalificationCommand(
-                            storedCalification.Period,
-                            storedCalification.EvaluatedEmployee,
-                            // The EvaluationCalifications document Id will be Evaluations/2015-06/evaluatedUsername/company in this case (Evaluations/2015-06/evaluatedUsername/evaluatorUsername in the other cases)
-                            // The only problem is if someday an employee is called C. Ompany (highly unlikely, but who knows)
-                            "company",
-                            storedCalification.TemplateId,
-                            CalificationType.Company,
-                            storedCalification.EvaluationId));
-                    }
-                }
+                    // If it's the responsible's or the company's calification, then the evaluation project should be updated [ TODO: Improvement - Check if the project has changed before updating it ]
+                    updateEvaluationProject = (storedCalification.Owner == CalificationType.Responsible || storedCalification.Owner == CalificationType.Company);
+                    // If it's the company's calification (or devolution), then the evaluation comments should be updated [ TODO: Improvement - Check if the comments have changed before updating it ]
+                    updateEvaluationComments = storedCalification.Owner == CalificationType.Company;
 
-                // If it's the company's calification (or the devolution), then the evaluation project should be updated [ TODO: Improvement - Check if the project has changed before updating it ]
-                if (storedCalification.Owner == CalificationType.Company)
-                {
-                    updateEvaluationComments = true;
-                    updateEvaluationProject = true;
+                    // If it's the responsible's calification and it's finished, then the company calification should be created
+                    CreateCompanyEvaluation(storedCalification);
                 }
             }
 
             // The evaluation gets updated only if the calification being updated is the company or the final one (devolution)
+            UpdateEvaluation(updateEvaluationComments, updateEvaluationProject, storedEvaluation);
+        }
+
+        private void UpdateCalification(UpdateCalificationDTO calification, EvaluationCalification storedCalification)
+        {
+            // Update the calification comments and values
+            storedCalification.Comments = calification.Comments;
+            storedCalification.Califications = calification.Items;
+
+            // Update the EvaluationCalification document in the collection (DB)
+            RavenSession.Store(storedCalification);
+        }
+
+        private void CreateCompanyEvaluation(EvaluationCalification storedCalification)
+        {
+            if (storedCalification.Owner == CalificationType.Responsible && storedCalification.Finished)
+            {
+                ExecuteCommand(new GenerateCalificationCommand(storedCalification.Period, storedCalification.EvaluatedEmployee, COMPANY, storedCalification.TemplateId, CalificationType.Company,
+                    storedCalification.EvaluationId));
+            }
+        }
+
+        private void UpdateEvaluation(bool updateEvaluationComments, bool updateEvaluationProject, EmployeeEvaluation storedEvaluation)
+        {
             if (updateEvaluationComments)
             {
-                // Update the evaluation comments
                 storedEvaluation.StrengthsComment = _updateEvaluation.Strengths;
                 storedEvaluation.ToImproveComment = _updateEvaluation.ToImprove;
                 storedEvaluation.ActionPlanComment = _updateEvaluation.ActionPlan;
 
                 if (updateEvaluationProject)
                 {
-                    // Update the evaluation project
                     storedEvaluation.Project = _updateEvaluation.Project;
                 }
 
                 // Update the EmployeeEvaluation document in the collection (DB)
                 RavenSession.Store(storedEvaluation);
             }
+        }
+
+        private bool CanUpdate(string loggedUser, EmployeeEvaluation evaluation, EvaluationCalification calification)
+        {
+            //Auto evaluator
+            if (loggedUser == evaluation.UserName)
+            {
+                if (calification.Owner == CalificationType.Auto && calification.EvaluatorEmployee == loggedUser) return true;
+            }
+
+            //Responsible
+            if (loggedUser == evaluation.ResponsibleId)
+            {
+                if (calification.Owner == CalificationType.Responsible && calification.EvaluatorEmployee == loggedUser) return true;
+
+                if (calification.Owner == CalificationType.Auto && evaluation.ReadyForDevolution) return true;
+
+                if (calification.Owner == CalificationType.Company && calification.EvaluatorEmployee == COMPANY) return true;
+            }
+
+            //Evaluator
+            if (calification.Owner == CalificationType.Evaluator && calification.EvaluatorEmployee == loggedUser) return true;
+
+            return false;
         }
     }
 }
