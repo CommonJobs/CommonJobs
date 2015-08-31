78cf8ea3e7c8f31f99d6591bac8b7d3c2db485d1
gkolocsar <gkolocsar@makingsense.com>
2015-08-31 12:17:29 -0300

    Update raven server version to the latest (v3)

---
+++ source/CommonJobs/Libs/RavenDB/CommonAssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Hibernating Rhinos")]
+[assembly: AssemblyCopyright("© Hibernating Rhinos 2004 - 2.3690. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle("RavenDB")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.3690.0")]
+[assembly: AssemblyInformationalVersion("3.0.0 / cdc39ac / ")]
+[assembly: AssemblyProduct("RavenDB")]
+[assembly: AssemblyDescription("RavenDB is a second generation LINQ enabled document database for .NET")]
+
+[assembly: CLSCompliant(true)]
+[assembly: SuppressIldasm()]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
