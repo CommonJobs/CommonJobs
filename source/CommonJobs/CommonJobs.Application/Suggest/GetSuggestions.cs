8f1d355fa0f985bc4b219fd6c4b8ff62bdfe97f5
gkolocsar <gkolocsar@makingsense.com>
2015-08-31 16:30:22 -0300

    Fix raven server files in project. Fix GetSuggestions command.

--- source/CommonJobs/CommonJobs.Application/Suggest/GetSuggestions.cs
+++ source/CommonJobs/CommonJobs.Application/Suggest/GetSuggestions.cs
@@ -26,43 +26,58 @@ namespace CommonJobs.Application.Suggestions
 
         public override IEnumerable<string> Execute()
         {
-            var query = RavenSession.Query<Persons_Suggestions.Projection, Persons_Suggestions>()
+            RavenQueryStatistics stats;
+
+            var q = RavenSession.Query<Persons_Suggestions.Projection, Persons_Suggestions>()
+                .Statistics(out stats)
+                .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite())
                 .Search(FieldSelector, Term.TrimEnd('*', '?') + "*", escapeQueryOptions: EscapeQueryOptions.AllowPostfixWildcard)
-                .Select(FieldSelector)
-                .As<string>()
+                //.Select(FieldSelector)
+                //.As<string>()
                 .Distinct()
                 .Take(MaxSuggestions * 2); //Padding porque no puedo filtrar los vacíos antes
 
-            var results = query.AsEnumerable()
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .OrderBy(x => x)
-                .Take(MaxSuggestions).ToList();
+            var query = q.ToList();
 
-            if (results.Count < MaxSuggestions)
-            {
-                var suggestionTerms = query.Suggest().Suggestions.Where(x => x != Term).ToArray();
-                if (suggestionTerms.Length > 0)
-                {
-                    var extraQuery = RavenSession.Query<Persons_Suggestions.Projection, Persons_Suggestions>()
-                        .Search(FieldSelector, string.Join(" ", suggestionTerms))
-                        .Select(FieldSelector)
-                        .As<string>()
-                        .Distinct()
-                        .Take((MaxSuggestions - results.Count) * 2); //Padding porque no puedo filtrar los vacíos y duplicados antes
+            var results = query.Select(FieldSelector.Compile()).ToList();
 
-                    var extraResults = extraQuery.ToList();
+            //var query = RavenSession.Query<Persons_Suggestions.Projection, Persons_Suggestions>()
+            //    .Search(FieldSelector, Term.TrimEnd('*', '?') + "*", escapeQueryOptions: EscapeQueryOptions.AllowPostfixWildcard)
+            //    .Select(FieldSelector)
+            //    .As<string>()
+            //    .Distinct()
+            //    .Take(MaxSuggestions * 2); //Padding porque no puedo filtrar los vacíos antes
 
-                    extraResults = extraResults
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Where(x => !results.Contains(x))
-                        .Take(MaxSuggestions - results.Count)
-                        .ToList();
+            //var results = query.AsEnumerable()
+            //    .Where(x => !string.IsNullOrWhiteSpace(x))
+            //    .OrderBy(x => x)
+            //    .Take(MaxSuggestions).ToList();
 
-                    results.AddRange(extraResults);
-                }
-            }
+            //if (results.Count < MaxSuggestions)
+            //{
+            //    var suggestionTerms = q.Suggest().Suggestions.Where(x => x != Term).ToArray();
+            //    if (suggestionTerms.Length > 0)
+            //    {
+            //        var extraQuery = RavenSession.Query<Persons_Suggestions.Projection, Persons_Suggestions>()
+            //            .Search(FieldSelector, string.Join(" ", suggestionTerms))
+            //            .Select(FieldSelector)
+            //            .As<string>()
+            //            .Distinct()
+            //            .Take((MaxSuggestions - results.Count) * 2); //Padding porque no puedo filtrar los vacíos y duplicados antes
 
-            return results;
+            //        var extraResults = extraQuery.ToList();
+
+            //        extraResults = extraResults
+            //            .Where(x => !string.IsNullOrWhiteSpace(x))
+            //            .Where(x => !results.Contains(x))
+            //            .Take(MaxSuggestions - results.Count)
+            //            .ToList();
+
+            //        results.AddRange(extraResults);
+            //    }
+            //}
+
+            return results.Select(x => x.ToString()).ToList();
         }
     }
 }
