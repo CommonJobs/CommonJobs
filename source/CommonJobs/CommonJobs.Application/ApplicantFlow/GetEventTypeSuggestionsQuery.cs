bd3ed26963e2a25d21a959b50bc88bf32c5566e0
gkolocsar <gkolocsar@makingsense.com>
2015-09-03 16:49:36 -0300

    Fix (not good enough but at least won't break the app) suggestions.

--- source/CommonJobs/CommonJobs.Application/ApplicantFlow/GetEventTypeSuggestionsQuery.cs
+++ source/CommonJobs/CommonJobs.Application/ApplicantFlow/GetEventTypeSuggestionsQuery.cs
@@ -24,42 +24,58 @@ namespace CommonJobs.Application.ApplicantFlow
 
         public override IEnumerable<string> Execute()
         {
-            var query = RavenSession.Query<ApplicantEventType_Suggestions.Projection, ApplicantEventType_Suggestions>()
+            RavenQueryStatistics stats;
+
+            var q = RavenSession.Query<ApplicantEventType_Suggestions.Projection, ApplicantEventType_Suggestions>()
+                .Statistics(out stats)
+                .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite())
                 .Search(x => x.Text, Term.TrimEnd('*', '?') + "*", escapeQueryOptions: EscapeQueryOptions.AllowPostfixWildcard)
                 .OrderByDescending(x => x.Predefined).ThenBy(x => x.Text)
-                .Select(x => x.Text)
-                .As<string>()
+                //.Select(FieldSelector)
+                //.As<string>()
                 .Distinct()
                 .Take(MaxSuggestions * 2); //Padding porque no puedo filtrar los vacíos antes
 
-            var results = query.AsEnumerable()
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .Take(MaxSuggestions).ToList();
+            var query = q.ToList();
 
-            if (results.Count < MaxSuggestions)
-            {
-                var suggestionTerms = query.Suggest().Suggestions.Where(x => x != Term).ToArray();
-                if (suggestionTerms.Length > 0)
-                {
-                    var extraQuery = RavenSession.Query<ApplicantEventType_Suggestions.Projection, ApplicantEventType_Suggestions>()
-                        .Search(x => x.Text, string.Join(" ", suggestionTerms))
-                        .OrderByDescending(x => x.Predefined).ThenBy(x => x.Text)
-                        .Select(x => x.Text)
-                        .As<string>()
-                        .Distinct()
-                        .Take((MaxSuggestions - results.Count) * 2); //Padding porque no puedo filtrar los vacíos y duplicados antes
+            var results = query.Select(x => x.Text).ToList();
 
-                    var extraResults = extraQuery.ToList();
+            //var query = RavenSession.Query<ApplicantEventType_Suggestions.Projection, ApplicantEventType_Suggestions>()
+            //    .Search(x => x.Text, Term.TrimEnd('*', '?') + "*", escapeQueryOptions: EscapeQueryOptions.AllowPostfixWildcard)
+            //    .OrderByDescending(x => x.Predefined).ThenBy(x => x.Text)
+            //    .Select(x => x.Text)
+            //    .As<string>()
+            //    .Distinct()
+            //    .Take(MaxSuggestions * 2); //Padding porque no puedo filtrar los vacíos antes
 
-                    extraResults = extraResults
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Where(x => !results.Contains(x))
-                        .Take(MaxSuggestions - results.Count)
-                        .ToList();
+            //var results = query.AsEnumerable()
+            //    .Where(x => !string.IsNullOrWhiteSpace(x))
+            //    .Take(MaxSuggestions).ToList();
 
-                    results.AddRange(extraResults);
-                }
-            }
+            //if (results.Count < MaxSuggestions)
+            //{
+            //    var suggestionTerms = query.Suggest().Suggestions.Where(x => x != Term).ToArray();
+            //    if (suggestionTerms.Length > 0)
+            //    {
+            //        var extraQuery = RavenSession.Query<ApplicantEventType_Suggestions.Projection, ApplicantEventType_Suggestions>()
+            //            .Search(x => x.Text, string.Join(" ", suggestionTerms))
+            //            .OrderByDescending(x => x.Predefined).ThenBy(x => x.Text)
+            //            .Select(x => x.Text)
+            //            .As<string>()
+            //            .Distinct()
+            //            .Take((MaxSuggestions - results.Count) * 2); //Padding porque no puedo filtrar los vacíos y duplicados antes
+
+            //        var extraResults = extraQuery.ToList();
+
+            //        extraResults = extraResults
+            //            .Where(x => !string.IsNullOrWhiteSpace(x))
+            //            .Where(x => !results.Contains(x))
+            //            .Take(MaxSuggestions - results.Count)
+            //            .ToList();
+
+            //        results.AddRange(extraResults);
+            //    }
+            //}
 
             return results;
         }
--- source/CommonJobs/CommonJobs.Application/EmployeeAbsences/GetSuggestions.cs
+++ source/CommonJobs/CommonJobs.Application/EmployeeAbsences/GetSuggestions.cs
@@ -24,42 +24,21 @@ namespace CommonJobs.Application.EmployeeAbsences
 
         public override IEnumerable<string> Execute()
         {
-            var query = RavenSession.Query<EmployeeAbsences_Suggestions.Projection, EmployeeAbsences_Suggestions>()
+            RavenQueryStatistics stats;
+
+            var q = RavenSession.Query<EmployeeAbsences_Suggestions.Projection, EmployeeAbsences_Suggestions>()
+                .Statistics(out stats)
+                .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite())
                 .Search(x => x.Text, Term.TrimEnd('*', '?') + "*", escapeQueryOptions: EscapeQueryOptions.AllowPostfixWildcard)
                 .OrderByDescending(x => x.Predefined).ThenBy(x => x.Text)
-                .Select(x => x.Text)
-                .As<string>()
+                //.Select(FieldSelector)
+                //.As<string>()
                 .Distinct()
                 .Take(MaxSuggestions * 2); //Padding porque no puedo filtrar los vacíos antes
 
-            var results = query.AsEnumerable()
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .Take(MaxSuggestions).ToList();
+            var query = q.ToList();
 
-            if (results.Count < MaxSuggestions)
-            {
-                var suggestionTerms = query.Suggest().Suggestions.Where(x => x != Term).ToArray();
-                if (suggestionTerms.Length > 0)
-                {
-                    var extraQuery = RavenSession.Query<EmployeeAbsences_Suggestions.Projection, EmployeeAbsences_Suggestions>()
-                        .Search(x => x.Text, string.Join(" ", suggestionTerms))
-                        .OrderByDescending(x => x.Predefined).ThenBy(x => x.Text)
-                        .Select(x => x.Text)
-                        .As<string>()
-                        .Distinct()
-                        .Take((MaxSuggestions - results.Count) * 2); //Padding porque no puedo filtrar los vacíos y duplicados antes
-
-                    var extraResults = extraQuery.ToList();
-
-                    extraResults = extraResults
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Where(x => !results.Contains(x))
-                        .Take(MaxSuggestions - results.Count)
-                        .ToList();
-
-                    results.AddRange(extraResults);
-                }
-            }
+            var results = query.Select(x => x.Text).ToList();
 
             return results;
         }
