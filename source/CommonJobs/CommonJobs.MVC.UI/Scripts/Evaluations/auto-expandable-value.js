68c923e41fc544ecb3b2d4a5160de87335a52164
Abriscioli <abriscioli@makingsense.com>
2016-06-22 14:58:28 -0300

    Apply comments and suggestions

---
+++ source/CommonJobs/CommonJobs.MVC.UI/Scripts/Evaluations/auto-expandable-value.js
@@ -0,0 +1,16 @@
+﻿(function ($, ko) {
+    ko.bindingHandlers.autoExpandableValue = {
+        init: function (element, valueAccessor, allBindingsAccessor) {
+            var value = valueAccessor();
+            if (ko.isObservable(value)) {
+                element.value = value();
+            }
+            ko.bindingHandlers.value.init(element, valueAccessor, allBindingsAccessor);
+        },
+        update: function (element, valueAccessor) {
+            ko.bindingHandlers.value.update(element, valueAccessor);
+            $(element).height(1);
+            $(element).height(25 + element.scrollHeight);
+        }
+    };
+})(jQuery, this.ko)
--- source/CommonJobs/CommonJobs.MVC.UI/Scripts/Evaluations/height-updater.js
+++
@@ -1,8 +0,0 @@
-﻿(function ($, ko) {
-    ko.bindingHandlers.heightUpdater = {
-        update: function (element) {
-            $(element).height(1);
-            $(element).height(25 + element.scrollHeight);
-        },
-    };
-})(jQuery, this.ko)
