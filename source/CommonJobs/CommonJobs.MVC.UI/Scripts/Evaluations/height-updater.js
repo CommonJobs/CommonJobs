14ce09fae9961cdd8afd8949a771d7ecb37683dc
Abriscioli <abriscioli@makingsense.com>
2016-06-22 11:48:11 -0300

    Make "projects" field auto-expandable

---
+++ source/CommonJobs/CommonJobs.MVC.UI/Scripts/Evaluations/height-updater.js
@@ -0,0 +1,8 @@
+﻿(function ($, ko) {
+    ko.bindingHandlers.heightUpdater = {
+        update: function (element) {
+            $(element).height(1);
+            $(element).height(25 + element.scrollHeight);
+        },
+    };
+})(jQuery, this.ko)
