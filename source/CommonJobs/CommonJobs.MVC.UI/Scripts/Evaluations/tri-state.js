c1963cc977c7f2ff40ff447f467b351c786e1722
Abriscioli <abriscioli@makingsense.com>
2016-06-28 16:20:10 -0300

    Add a switch button with tri-states to show or hide all comments

---
+++ source/CommonJobs/CommonJobs.MVC.UI/Scripts/Evaluations/tri-state.js
@@ -0,0 +1,29 @@
+﻿(function ($, ko) {
+    ko.bindingHandlers.triState = {
+        update: function (element, valueAccessor) {
+            var value = valueAccessor()();
+            var input = $(element).children("input");
+            if ($(input).length == 1 && $(input).prop("type") == "checkbox") {
+                switch (value) {
+                    case true:
+                        $(element).before().removeClass("indeterminate");
+                        $(element).before().addClass("checked");
+                        $(input).prop("indeterminate", false);
+                        $(input).prop("checked", true);
+                        break;
+                    case false:
+                        $(element).before().removeClass("checked");
+                        $(element).before().removeClass("indeterminate");
+                        $(input).prop("indeterminate", false);
+                        $(input).prop("checked", false);
+                        break;
+                    case null:
+                        $(element).before().addClass("indeterminate");
+                        $(input).prop("indeterminate", true);
+                        $(input).prop("checked", true);
+                        break;
+                }
+            }
+        },
+    };
+})(jQuery, this.ko)
