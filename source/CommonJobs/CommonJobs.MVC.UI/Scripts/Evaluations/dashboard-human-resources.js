de78cb33e0d680abf1b8cf843d67e2e4df9d59be
Abriscioli <abriscioli@makingsense.com>
2016-07-18 10:07:30 -0300

    Add the shared links column to show or edit shared links
    Add the shared links column to show or edit shared links

--- source/CommonJobs/CommonJobs.MVC.UI/Scripts/Evaluations/dashboard-human-resources.js
+++ source/CommonJobs/CommonJobs.MVC.UI/Scripts/Evaluations/dashboard-human-resources.js
@@ -11,7 +11,7 @@
         { title: 'Empleado', sortable: true, sortPropertyName: 'fullName', defaultPropertyName: 'fullName', asc: true, activeSort: ko.observable(false) },
         { title: 'Puesto', sortable: true, sortPropertyName: 'currentPosition', defaultPropertyName: 'fullName', asc: true, activeSort: ko.observable(false) },
         { title: 'Seniority', sortable: true, sortPropertyName: 'seniority', defaultPropertyName: 'fullName', asc: true, activeSort: ko.observable(false) },
-        { title: 'Links', sortable: false, defaultPropertyName: 'fullName'},
+        { title: 'Links', sortable: false, defaultPropertyName: 'fullName' },
         { title: 'Responsable', sortable: true, sortPropertyName: 'responsibleId', defaultPropertyName: 'fullName', asc: true, activeSort: ko.observable(false) },
         { title: 'Estado', sortable: true, sortPropertyName: 'state', defaultPropertyName: 'fullName', asc: true, activeSort: ko.observable(false) },
         { title: '', sortable: false }
@@ -39,6 +39,7 @@
         this.id = '';
         this.isResponsible = false;
         this.isEvaluationManager = false;
+        this.isEvaluationEditable = ko.observable(true);
         this.isEditable = false;
         this.responsibleId = ko.observable('');
         this.fullName = '';
@@ -48,13 +49,14 @@
         this.seniority = '';
         this.state = ko.observable('');
         this.currentState = '';
-        this.sharedLinks = [];
+        this.sharedLinks = ko.observableArray();
         if (data) {
             this.fromJs(data);
         }
     }
 
     EvaluationReport.prototype.fromJs = function (data) {
+        var self = data;
         this.id = data.Id;
         this.isResponsible = data.IsResponsible;
         this.isEvaluationManager = window.ViewData.isEvaluationManager;
@@ -67,13 +69,17 @@
         this.seniority = data.Seniority || '';
         this.state(data.State);
         this.stateName = evaluationStates[this.state()];
-        this.sharedLinks = _.map(data.SharedLinks, function (sharedLink) {
+        this.sharedLinks(_.map(data.SharedLinks, function (sharedLink) {
             return {
-                link: urlGenerator.sharedAction(this.period + "/" + this.userName + "/", "Evaluations", null, sharedLink.SharedCode),
-                friendlyName: sharedLink.FriendlyName,
-                expirationDate: sharedLink.ExpirationDate
+                link: urlGenerator.sharedAction(self.Period + "/" + self.UserName, "Evaluations", null, sharedLink.SharedCode),
+                period: self.Period,
+                userName: self.UserName,
+                sharedCode: sharedLink.SharedCode,
+                friendlyName: ko.observable(sharedLink.FriendlyName),
+                expirationDate: ko.observable(sharedLink.ExpirationDate),
+                edittingFriendlyName: ko.observable(false)
             }
-        })
+        }));
         this.stateClasses = "state-doc state-" + this.state();
         this.calificationUrl = urlGenerator.action(this.period + "/" + this.userName + "/", "Evaluations");
         this.showResponsibleManager = function (data, event) {
@@ -97,6 +103,67 @@
             UserName: this.userName,
             Period: this.period
         };
+    }
+
+    EvaluationReport.prototype.createLink = function (data, event) {
+        $.post("/Evaluations/api/CreateEvaluationSharedLink/" + this.period + "/" + this.userName)
+        .success(function (sharedLink) {
+            var newSharedLink = {
+                link: urlGenerator.sharedAction(this.period + "/" + this.userName, "Evaluations", null, sharedLink.SharedCode),
+                period: this.period,
+                userName: this.userName,
+                friendlyName: ko.observable(sharedLink.FriendlyName),
+                sharedCode: sharedLink.SharedCode,
+                expirationDate: ko.observable(new Date(sharedLink.ExpirationDate).toLocaleDateString()),
+                edittingFriendlyName: ko.observable(false)
+            };
+            data.sharedLinks.push(newSharedLink);
+        })
+        .fail(function () {
+            alert('Fallo interno. Por favor recargue la página.');
+        });
+    }
+
+    EvaluationReport.prototype.updateSharedLink = function (data, event) {
+        viewmodel.isLoading(true);
+        var updatedSharedLink = sharedLinkToDto(data)
+        $.ajax("/Evaluations/api/UpdateEvaluationSharedLink/", {
+            type:"POST",
+            dataType: "text",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(updatedSharedLink)
+        })
+        .success(function () {
+            data.edittingFriendlyName(false);
+        })
+        .fail(function (error, textStatus) {
+            alert('Fallo interno. Por favor recargue la página.');
+        })
+        .always(function () {
+            viewmodel.isLoading(false);
+        });
+    }
+
+    SharedLink.prototype.deleteSharedLink = function (data, event) {
+        viewmodel.isLoading(true);
+        $.ajax("/Evaluations/api/DeleteEvaluationSharedLink/", {
+            type: "POST",
+            dataType: "text",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data.toDto())
+        })
+        .success(function () {
+            var evaluation = _.find(viewmodel.items(),function (eval) {
+                return eval.userName == data.userName;
+           });
+           evaluation.sharedLinks(_.without(evaluation.sharedLinks(), data));
+        })
+        .fail(function (error, textStatus) {
+            alert('Fallo interno. Por favor recargue la página.');
+        })
+        .always(function () {
+            viewmodel.isLoading(false);
+        });
     }
 
     var ResponsibleManager = function (data) {
@@ -149,6 +216,18 @@
     $("#selectedPeriod").change(function () {
         window.location = this.value;
     })
+
+    function sharedLinkToDto(updatedSharedLink) {
+        return {
+            period: updatedSharedLink.period,
+            userName: updatedSharedLink.userName,
+            sharedLink: {
+                FriendlyName: updatedSharedLink.friendlyName(),
+                SharedCode: updatedSharedLink.sharedCode,
+                ExpirationDate: updatedSharedLink.expirationDate()
+            }
+        }
+    }
 
     function getReportDashboard(period) {
         viewmodel.isLoading(true);
