bcf7a80459dffac150f9e14dc57971501384bb4a
Andres Moschini <andresmoschini@gmail.com>
2017-08-29 15:03:29 -0300

    Move some configurations from .json file to default

--- source/CommonJobs/Admin/ExportToZoho/ZohoConfiguration.cs
+++ source/CommonJobs/Admin/ExportToZoho/ZohoConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace Admin.ExportToZoho
         public string Token { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string LoginUriTemplate { get; set; }
-        public string GeneralUriTemplate { get; set; }
+        public string LoginUriTemplate { get; set; } = "https://accounts.zoho.com/apiauthtoken/nb/create?SCOPE=ZohoRecruit/recruitapi&EMAIL_ID={username}&PASSWORD={password}&DISPLAY_NAME=recruit";
+        public string GeneralUriTemplate { get; set; } = "https://recruit.zoho.com/recruit/private/xml/{module}/{method}?authtoken={token}&scope=recruitapi&version=2";
     }
 }
